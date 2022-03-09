e91e1bb4d9d099425065347d48380f3994afe499
SlaughterBlade <70080836+SlaughterBlade@users.noreply.github.com>
2022-03-09 16:23:52 +0300

    Added the basic template

---
+++ index.js
@@ -0,0 +1,48 @@
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// Replying to commands
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+	else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	}
+	else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+
+// Server info command
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+	else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	}
+	else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+
+// Login to Discord with your client's token
+client.login(token);
