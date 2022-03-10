c7e05b6f15b046635efc8f31396b881bef99cc09
SlaughterBlade <70080836+SlaughterBlade@users.noreply.github.com>
2022-03-10 14:16:16 +0300

    🚧 added ping command
    check the Roundtrip latency with /ping.

---
+++ commands/ping.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+
+// Check the bot ms
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Test bot latency'),
+	async execute(interaction) {
+		const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+		interaction.editReply(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
+	},
+};
+
