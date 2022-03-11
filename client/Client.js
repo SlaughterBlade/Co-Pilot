7da106bfcf39b5b6a5c2dde08136c1ad5b2a56ce
SlaughterBlade <70080836+SlaughterBlade@users.noreply.github.com>
2022-03-11 05:02:21 +0300

    🚧 Cleaner Code

---
+++ client/Client.js
@@ -0,0 +1,13 @@
+const { Client, Collection, Intents } = require('discord.js');
+
+module.exports = class extends Client {
+	constructor(config) {
+		super({
+			intents: [Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS],
+		});
+
+		this.commands = new Collection();
+
+		this.config = config;
+	}
+};
