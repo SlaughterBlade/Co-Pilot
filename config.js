41235e36359bf163af7329d143a14e9c9f74abf6
SlaughterBlade <70080836+SlaughterBlade@users.noreply.github.com>
2022-03-22 05:26:44 +0300

    🚧 More Work !

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
 
 	//	These values will be ignored if you have set the environment variables (must be in uppercase)
 
-	prefix: '', // required, command prefix
+	prefix: '!', // required, command prefix
 	botToken: '', // required, https://discordjs.guide/preparations/setting-up-a-bot-application.html#your-token
 	clientId: '', // optional, the bot's client ID, leave empty to disable slash commands
 	geniusApiToken: '', // optional, but recommended for lyrics search - https://genius.com/api-clients
